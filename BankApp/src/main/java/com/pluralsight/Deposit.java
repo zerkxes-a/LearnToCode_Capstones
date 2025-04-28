e43846ccddf0304edf0a33db108faa97ce1d787e
zerkxes-a <annazerkle@gmail.com>
2025-04-28 16:07:23 -0400

    Completed deposit page and logic

--- BankApp/src/main/java/com/pluralsight/Deposit.java
+++ BankApp/src/main/java/com/pluralsight/Deposit.java
@@ -1,36 +1,19 @@
 package com.pluralsight;
 
-import java.time.LocalDateTime;
 
 public class Deposit {
     private String description;
     private String vendor;
     private float price;
+    //CONSTRUCTOR METHOD(USED TO CREATE INSTANCE OF OBJECT FROM INPUT IN MENU) AUTOMATICALLY SETS VALUES
     public Deposit(String description, String vendor, float price){
         this.description = description;
         this.vendor = vendor;
         this.price = price;
     }
-    public Deposit() {
-        this.description = "";
-        this.vendor = "";
-        this.price = 0.0f;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getVendor() {
-        return vendor;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
     @Override
+    //GETS ALL THE VALUES AND MAKES IT INTO A STRING
     public String toString() {
-        return String.format("%s %s %.2f", description, vendor,price);
+        return String.format("%s | %s | %.2f\n", description, vendor, price);
     }
 }
--- BankApp/src/main/java/com/pluralsight/Menu.java
+++ BankApp/src/main/java/com/pluralsight/Menu.java
@@ -1,17 +1,21 @@
 package com.pluralsight;
-
-import java.sql.SQLOutput;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
 
 public class Menu {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        run();
+        chooseScreen();
     }
 
     //MAIN MENU START
-    public static void run() {
+    public static void chooseScreen() {
         System.out.println("Welcome to your Personal Banking App! \n");
 
         //CREATE WHILE LOOP FOR HOMESCREEN MENU SELECTION
@@ -23,47 +27,82 @@ public class Menu {
             System.out.println("D) Add Deposit");
             System.out.println("P) Make a Payment (Debit)");
             System.out.println("L) Ledger");
-            System.out.println(("X) Exit \n"));
+            System.out.println("X) Exit \n");
 
             System.out.print("Please enter your letter selection here:  ");
 
-            switch (scanChar()) {
-                case 1: //GO TO FISRT PAGE (ADD DEPOSIT)
+            String choice = scanner.nextLine().toUpperCase();
+            switch (choice) {
+                case "D": //GO TO FIRST PAGE (ADD DEPOSIT)
                     System.out.println("Continue to Deposit info (Please press Enter):  ");
                     scanner.nextLine();
-                    //todo prompt user for deposit info to go to csv file
+                    depositInfo();
                     break;
-                case 2: //GO TO THE SECOND PAGE (MAKE A PAYMENT)
+                case "P": //GO TO THE SECOND PAGE (MAKE A PAYMENT)
                     System.out.println("Continue to Payment (Please press Enter):  ");
-                    scanner.nextLine();
+                    scanner.next();
                     //todo prompt user for debit info and save to csv file
                     break;
-                case 3: //CREATE LEDGER MENU TO CALL TO
-                    System.out.println("Continue to Ledger Menu (Please press Enter):  ");
+                case "L": //CREATE LEDGER MENU TO CALL TO
+                    System.out.println("Continue to Ledger Menu (Please press Enter): ");
                     scanner.nextLine();
                     // TODO displayLedger();  CREATE LEDGER MENU BELOW
-                case 4: //EXIT OUT OF APP
+                case "X": //EXIT OUT OF APP
                     System.exit(0);
                 default: //WHOOPSIE WAS DONE, REDO
                     System.out.println("Invalid option selected, please try again \n");
             }
-
-
         }
     }
+    //DEPOSIT SCREEN
+    private static void depositInfo() {
 
+        System.out.print("Please enter the Transaction Description: ");
+        String description = scanner.nextLine();
+        System.out.print("Please enter the Vendor Name: ");
+        String vendor = scanner.nextLine();
+        System.out.print("Please enter the Deposit Amount: ");
+        float price = scanner.nextFloat();
+        scanner.nextLine(); //CONSUME LINE CLRF
+        //research what this is Anna!!!!
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/transactions.csv", true))) {
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd | HH:mm:ss | ");
+            //WRITES DEPOSIT INFO TO TRANSACTIONS.CSV
+           bufferedWriter.write((now.format(formatter)) + new Deposit(description, vendor, price));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //PAYMENT SCREEN
+    //todo ALL OF IT DO IT ALL PLACEHOLDER FOR LATER!!!
+    private static void paymentInfo(){
 
+    }
+    //LEDGER SCREEN
+    private static void ledgerMenu(){
+        System.out.println("Welcome to the Leger Menu\n");
+        System.out.println("Please choose from the following options: \n");
+        System.out.println("A) All - display all transactions");
+        System.out.println("D) Deposits");
+        System.out.println("P) Payments");
+        System.out.println("R) Reports");
+        System.out.print("Please enter your selection here: ");
+
+    }
     // CONVIENCE METHODS GO HERE
-    private static char scanChar() {
-        while (!scanner.hasNextChar()) {
+    //convience method for choosing lettered screen choices
+    private static String scanChar () { //todo FUCKING DO BETTER MAKE STRING tHAT ONLY READS FIRST LETTER
+        while (!scanner.hasNext()) {
             scanner.nextLine();
             System.out.println("Please ONLY enter a single Letter");
-            System.out.print("Enter Letter here: ");
+            System.out.print("Enter Single Letter here: ");
         }
-        char choice = scanner.nextChar();
+        String choice = scanner.nextLine().trim().toUpperCase();
         scanner.nextLine();
         return choice;
     }
+    //convience method for reports choice screen
     private static int scanInt(){
         while(!scanner.hasNextInt()) {
             scanner.nextLine();
